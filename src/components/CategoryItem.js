5289076112d8c0362a156739f297ce468860f017
Jerika Kanz <jerikacapila@gmail.com>
2022-10-09 13:01:19 +0200

    Create Category Component

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { useState, useContext } from "react";
+import { ShoppingContext } from "../context/ShoppingContext";
+import ShoppingItem from "./ShoppingItem";
+
+export default function CategoryItem({ category }) {
+  const [showShoppingList, setShowShoppingList] = useState(true);
+
+  const { shoppingList } = useContext(ShoppingContext);
+
+  const toggleShoppingList = () => {
+    setShowShoppingList(!showShoppingList);
+  };
+
+  return (
+    <StyledCategoryItem>
+      <ListToggler>
+        <h3>{category.name.en}</h3>
+        <button type="button" onClick={toggleShoppingList}>
+          {showShoppingList ? "▴" : "▾"}
+        </button>
+      </ListToggler>
+
+      <List variant={showShoppingList}>
+        {shoppingList
+          .filter((item) => item.category._ref === category._id)
+          .map((item) => (
+            <ShoppingItem key={item._id} item={item} />
+          ))}
+      </List>
+    </StyledCategoryItem>
+  );
+}
+
+const StyledCategoryItem = styled.li`
+  width: 500px;
+
+  h3 {
+    margin: 0;
+    padding: 5px;
+  }
+`;
+
+const ListToggler = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin: auto;
+
+  button {
+    all: unset;
+    position: absolute;
+    right: 0;
+  }
+`;
+
+const List = styled.ul`
+  display: ${({ variant }) => (variant ? "flex" : "none")};
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 5px;
+  list-style: none;
+  padding: 0;
+`;
